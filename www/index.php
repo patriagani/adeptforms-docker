16e8f03862a0eed6cec73d3cceb6efb0d52cf08a
patriagani <patria.gani@gmail.com>
2020-08-27 14:48:22 +0700

    initial commit

---
+++ www/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="">
+	<title>Adeptform Dockerfile</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+	    crossorigin="anonymous">
+	<link href="cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center corps">
+	<div class="main">
+		<div class="alert alert-success" role="alert">
+			<div class="row justify-content-center">
+				<ul class="row col mt-3">
+					<li class="col">Ubuntu 18.04</li>
+					<li class="col">Apache2</li>
+					<li class="col">PHP 7.2</li>
+				</ul>
+			</div>
+			<hr>
+		</div>
+	</div>
+</body>
+
+</html>
+
+<?php phpinfo();
